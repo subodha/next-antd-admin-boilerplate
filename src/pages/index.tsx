f94f8f9ddbf2b48f2de3e2c5b7efa670fc157a54
Subodha Pathiraja <subodha@atomix.com.au>
2021-09-20 17:47:22 +0530

    refactor: adding husky commitlint and some configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from "next";
-
-import React from 'react'
+import type { NextPage } from 'next';
+import React from 'react';
 import { Button } from 'antd';
+
+const test = 'test';
 
 const Home: NextPage = () => {
   return (
